a47357d01f5088f4fdb267b7119b8365b24dce3b
Dennis E <git@destenson.digital>
2018-11-25 18:02:29 -0800

    accounts => accts

--- eth/backend.go
+++ eth/backend.go
@@ -320,8 +320,8 @@ func (s *Ethereum) Etherbase() (eb common.Address, err error) {
 		return etherbase, nil
 	}
 	if wallets := s.AccountManager().Wallets(); len(wallets) > 0 {
-		if accounts := wallets[0].Accounts(); len(accounts) > 0 {
-			etherbase := accounts[0].Address
+		if accts := wallets[0].Accounts(); len(accts) > 0 {
+			etherbase := accts[0].Address
 
 			s.lock.Lock()
 			s.etherbase = etherbase
